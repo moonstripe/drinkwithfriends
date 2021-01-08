1777cf525a00bdf30cfcf066252429f1de0772bf
Andrew Segre <asegre18@cmc.edu>
2021-01-07 19:20:06 -0800

    trimmed the phat

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import NavTabs from "./components/NavTabs";
 import AppHead from './components/AppHead';
 import BackgroundVideo from './components/BackgroundVideo';
-import { WrappedSignIn } from './components/SignIn';
-import { WrappedSignUp } from './components/SignUp';
 import Table from './components/Table';
-import Game from './components/Game'
-import SignInSignUp from './components/SignInOrSignUp';
-
-// You can also just do the code below
-// import {
-//   UserContainer,
-//   SignUp,
-// } from './pages';
-
-
-
 
 // import Contact from "./components/pages/Contact";
 
@@ -24,10 +10,7 @@ function App() {
   return (
     <Router>
       <AppHead/>
-      <Route path='/signup' component={WrappedSignUp}/>
-      <Route path='/signin' component={WrappedSignIn}/>
       <Route path="/game" component={Table}/>
-      <Route path="/gameScreen" component={Game}/>
       <Route exact path="/" component={BackgroundVideo}/>
     </Router>
   );
